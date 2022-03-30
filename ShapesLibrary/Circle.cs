a14a26e3f904454e52cdce98ffb86dd809ff5cce
Andrei Ogurtsov <ogurcov1997@mail.ru>
2022-03-30 11:25:13 +0300

    Добавление интерфейса и классов для вычисления площади круга и треугольника

---
+++ ShapesLibrary/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapesLibrary
+{
+    public class Circle : IShape
+    {
+        public double r { get; set; }
+        public Circle() { }
+        public Circle(double r)
+        {
+            this.r = r;
+        }
+
+        public double Square()
+        {
+            return Math.PI * Math.Pow(r, 2);
+        }
+    }
+}
